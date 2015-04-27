d454dcfd530aa3e16c5a3d912eb943ce4985fc44
임성광 <seongkwang.im@friberry.com>
2015-04-28 06:11:54 +0900

    setup.py 작성.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='letmescrape',
+    version='0.0.1',
+    packages=['letmescrape', 'letmescrape.spiders'],
+    url='',
+    license='',
+    author='Friberry',
+    author_email='',
+    description=''
+)
