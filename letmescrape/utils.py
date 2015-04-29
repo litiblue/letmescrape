6979747d2f4b627e6dbe523e3cdc1d8d4988bd1c
임성광 <seongkwang.im@friberry.com>
2015-04-30 03:05:59 +0900

    splashjs 이용 시 get_absolute_url의 결과가 splash server 기준으로 나오던 버그 해결.

--- letmescrape/utils.py
+++ letmescrape/utils.py
@@ -4,6 +4,9 @@ from scrapy.utils.response import get_base_url
 
 
 def get_absolute_url(response, relative_url):
+    if '_splash_processed' in response.meta:
+        url = response.meta['_splash_processed']['args']['url']
+        response = response.replace(url=url)
     base_url = get_base_url(response)
     absolute_url = urljoin(base_url, relative_url)
     return absolute_url
