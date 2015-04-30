751c322b3d39a9f5195b069d8a88b00de72e02b5
Wonyoung Choi <wychoi@blissoft.com>
2015-04-30 20:34:49 +0900

    [gnc] GNC 카테고리 스파이더 구현

---
+++ letmescrape/spiders/gnc_category.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from base import CategorySpider
+from letmescrape.loaders import CategoryLoader
+from letmescrape.processors import JoinExcludingEmptyValues
+
+
+class GncCategorySpider(CategorySpider):
+    name = "gnc_category"
+    allowed_domains = ["gnc.com"]
+    start_urls = (
+        'http://www.gnc.com/',
+    )
+
+    def parse(self, response):
+        def _generate_loader(selector):
+            loader = CategoryLoader(selector=selector, response=response)
+            loader.title_out = JoinExcludingEmptyValues(' ')
+            loader.add_xpath('title', 'text()')
+            loader.add_xpath('title', 'span/text()')
+            loader.add_xpath('title', 'a/@title')
+            loader.add_xpath('link', '@href')
+            loader.add_xpath('link', 'a/@href')
+            return loader
+
+        for top_level_sel in response.xpath('//table[@id="mainNav"]//td[@class="mainNavItem"]'):
+            top_level_category_loader = _generate_loader(top_level_sel)
+            parent_category_loader = top_level_category_loader
+
+            for column_sel in top_level_sel.xpath('div[@class="mainNavSubContainer"]/ul[contains(@class, "mainNavSub")]/li[contains(@class, "subCats")]'):
+                category_loader = _generate_loader(column_sel.xpath('ul/li[@class="title"]'))
+
+                for leaf_sel in column_sel.xpath('ul/li[contains(@class, "subCat")]'):
+                    leaf_loader = _generate_loader(leaf_sel)
+                    category_loader.add_value('sub_categories', leaf_loader.load_item())
+
+                parent_category_loader.add_value('sub_categories', category_loader.load_item())
+
+            yield top_level_category_loader.load_item()
