3b50f347a04de6f6e98e5ce99c4c27714796fe8d
kwonno.lee <kwonno.lee@friberry.com>
2015-05-26 18:53:41 +0900

    [gymboree] 짐보리 카테고리 스파이더 구현.

---
+++ letmescrape/spiders/gymboree_category.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+from base import CategorySpider
+from letmescrape.loaders import CategoryLoader
+from letmescrape.processors import JoinExcludingEmptyValues
+from scrapy.http import Request
+
+
+class GymboreeCategorySpider(CategorySpider):
+    name = "gymboree_category"
+    allowed_domains = ["gymboree.com"]
+    start_urls = (
+        'http://www.gymboree.com/',
+    )
+
+    def _generate_loader(self, selector, response, title, link):
+            loader = CategoryLoader(selector=selector, response=response)
+            loader.title_out = JoinExcludingEmptyValues(' ')
+
+            loader.add_value('title', title)
+            if link is not None:
+                link = str(link).split('\'')[1].split('&')[0]
+                loader.add_value('link', link)
+
+            return loader
+
+    def parse(self, response):
+        for top_level_sel in response.xpath('//div[@id="top-main-menu"]/ul/li[node()]/a'):
+            title = top_level_sel.xpath('@title').extract()
+            link = top_level_sel.xpath('@href').extract()
+
+            top_level_category_loader = self._generate_loader(top_level_sel, response, title, link)
+
+            request = Request(url=link[0], callback=self.parse_category)
+            request.meta['loader'] = top_level_category_loader
+
+            yield request
+
+    def parse_category(self, response):
+
+        top_level_category_loader = response.meta['loader']
+        i = 1
+        for top_level_sub_column_sel in response.xpath('//div[@id="left-menu"]/h3[contains(@id,"dept")]/a/b'):
+            title = top_level_sub_column_sel.xpath('text()').extract()
+            link = None
+            top_level_sub_column_sel_loader = self._generate_loader(top_level_sub_column_sel, response, title, link)
+
+            sub_category = response.selector.xpath('//div[contains(@id,"left-menu")]/ul[@id="left-menu-ul%d"]/li[node()]/a' % i).extract()
+
+            if len(sub_category) == 0:
+                i += 1
+            j = 1
+
+            for column_sel in response.xpath('//div[contains(@id,"left-menu")]/ul[@id="left-menu-ul%d"]/li[node()]/a' % i):
+                title = column_sel.xpath('text()').extract()
+                link = column_sel.xpath('@href').extract()
+
+                column_sel_loader = self._generate_loader(column_sel, response, title, link)
+
+                for leaf_sel in response.xpath('//div[@id="left-menu"]/ul[@id="left-menu-ul%d"]/ul[@id="left-submenu-ul%d%d"]/li[node()]/a' % (i, i, j)):
+                    title = leaf_sel.xpath('text()').extract()
+                    link = leaf_sel.xpath('@href').extract()
+
+                    leaf_sel_loader = self._generate_loader(leaf_sel, response, title, link)
+                    column_sel_loader.add_value('sub_categories', leaf_sel_loader.load_item())
+
+                top_level_sub_column_sel_loader.add_value('sub_categories', column_sel_loader.load_item())
+                j += 1
+
+            top_level_category_loader.add_value('sub_categories', top_level_sub_column_sel_loader.load_item())
+            i += 1
+
+
+        yield top_level_category_loader.load_item()
+
+
