5959f257bbacedb3238a8fe3f9ceb92f0548f3cf
kwonno.lee <kwonno.lee@friberry.com>
2015-06-04 17:43:47 +0900

    [ralphlauren] 랄프로렌 카테고리 lua 스크립트 적용, required=True Error로 인한 수정.

--- letmescrape/spiders/ralphlauren_category.py
+++ letmescrape/spiders/ralphlauren_category.py
@@ -3,27 +3,38 @@
 from base import CategorySpider
 from letmescrape.loaders import CategoryLoader
 from letmescrape.processors import JoinExcludingEmptyValues
-
+from letmescrape.scripts import make_lua_script
 from scrapy import Request
 
 class RalphlaurenCategorySpider(CategorySpider):
     name = "ralphlauren_category"
-    # allowed_domains = ["ralphlauren.com"]
+    allowed_domains = ["ralphlauren.com"]
     start_urls = (
-        'http://www.ralphlauren.com',
+        'http://www.ralphlauren.com/home/index.jsp?geos=1',
     )
 
     def parse(self, response):
 
-        splash = 'http://192.168.59.103:8050/render.html?wait=1&render_all=1&url=http://www.ralphlauren.com/home/index.jsp?geos=1'
-        request = Request(splash, callback=self.parse_redirect_url)
-        request.meta['original_url'] = 'http://www.ralphlauren.com/home/index.jsp?geos=1'
+        script = make_lua_script('li[rel=shop-all-men]')
+        request = Request(response.url, callback=self.parse_redirect_url, meta={
+            'splash': {
+                    'endpoint': 'execute',
+                    'args': {'lua_source': script}
+            }
+        })
 
         yield request
 
+    def sale_category_loader(self, selector, response, title, link):
+        loader = CategoryLoader(selector=selector, response=response)
+        loader.title_out = JoinExcludingEmptyValues(' ')
+        loader.add_value('title', title)
+        loader.add_value('link', link)
+
+        return loader
+
+
     def parse_redirect_url(self, response):
-        url = response.meta['original_url']
-        response = response.replace(url=url)
 
         def _generate_loader(selector):
             loader = CategoryLoader(selector=selector, response=response)
@@ -31,13 +42,18 @@ class RalphlaurenCategorySpider(CategorySpider):
             loader.add_xpath('title', '@title')
             loader.add_xpath('title', 'h3/text()')
             loader.add_xpath('title', 'a/text()')
+            loader.add_xpath('title', 'a/font/b/text()')
             loader.add_xpath('link', '@href')
             loader.add_xpath('link', 'a/@href')
 
             return loader
 
-        for top_level_sel in response.xpath('//ul[@id="global-nav"]/li[contains(@class, "navitem")][node()]'):
-            top_level_category_loader =_generate_loader(top_level_sel.xpath('a[not(contains(@class,"top-nav last"))]'))
+        for top_level_sel in response.xpath('//ul[@id="global-nav"]/li[contains(@class, "navitem  more")]'):
+            exist_category = top_level_sel.xpath('a[not(contains(@class,"top-nav last"))]').extract()
+
+            if len(exist_category) != 0:
+                top_level_category_loader =_generate_loader(top_level_sel.xpath('a[not(contains(@class,"top-nav last"))]'))
+
             parent_category_loader = top_level_category_loader
 
             for column_sel in top_level_sel.xpath('div/div'):
@@ -47,8 +63,13 @@ class RalphlaurenCategorySpider(CategorySpider):
                     category_loader = _generate_loader(column_sel)
 
                     for leaf_sel in column_sel.xpath('ul[@class="nav-items"]/li'):
-                        leaf_loader = _generate_loader(leaf_sel)
-                        category_loader.add_value('sub_categories', leaf_loader.load_item())
+
+                        exist_leaf_sel_title_text = leaf_sel.xpath('a/text()').extract()
+                        exist_sale_category = leaf_sel.xpath('a/font/b/text()').extract()
+
+                        if len(exist_leaf_sel_title_text) != 0 or len(exist_sale_category):
+                            leaf_loader = _generate_loader(leaf_sel)
+                            category_loader.add_value('sub_categories', leaf_loader.load_item())
 
                     parent_category_loader.add_value('sub_categories', category_loader.load_item())
 
