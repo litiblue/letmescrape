41e1a34cc9f9d1363b35e31822144f49ccd3ea4c
kwonno.lee <kwonno.lee@friberry.com>
2015-06-09 12:22:43 +0900

    lua_script 강화, selectors, bitwise_operator 추가.

--- letmescrape/scripts.py
+++ letmescrape/scripts.py
@@ -1,9 +1,18 @@
 
-def make_lua_script(selector):
+def make_lua_script(selectors, bitwise_operators=""):
+
+    lua_string = """"""
+    for index, selector in enumerate(selectors):
+
+        if index == 0:
+            lua_string = lua_string + 'document.querySelector("' + selector + '") == null '
+        else:
+            lua_string = lua_string + bitwise_operators + ' document.querySelector("' + selector + '") == null '
+
     script = """
             function main(splash)
                 splash:go(splash.args.url)
-                while(splash:evaljs("document.querySelector('"""+selector+"""') == null"))
+                while(splash:evaljs('"""+lua_string+"""'))
                 do
                     splash:wait(0.05)
                 end
@@ -11,3 +20,4 @@ def make_lua_script(selector):
             end
             """
     return script
+
