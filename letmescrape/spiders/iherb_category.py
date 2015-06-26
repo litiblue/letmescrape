2e53c4bc13331e02dfb11f6a8665272fbc0f3b07
Wonyoung Choi <wonyoung.choi@friberry.com>
2015-06-26 15:31:33 +0900

    [iherb] 아이허브 카테고리 스파이더 구현

---
+++ letmescrape/spiders/iherb_category.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from scrapy import Request
+
+from base import CategorySpider
+from letmescrape.loaders import CategoryLoader
+from letmescrape.utils import get_absolute_url
+
+
+class IherbCategorySpider(CategorySpider):
+    name = "iherb_category"
+    allowed_domains = ["iherb.com"]
+    start_urls = (
+        'http://www.iherb.com/',
+    )
+
+    def generate_loader(self, selector, response):
+        loader = CategoryLoader(selector=selector, response=response)
+        loader.add_xpath('title', 'text()')
+        loader.add_xpath('link', '@href')
+        return loader
+
+    def parse(self, response):
+        for top_level_sel in response.xpath('//nav[@id="iherb-main-nav"]/div/ul/li/a'):
+            top_level_category_loader = self.generate_loader(top_level_sel, response)
+            url = get_absolute_url(response, top_level_sel.xpath('@href').extract()[0])
+            request = Request(url, callback=self.parse_sub)
+            request.meta['top_level_category_loader'] = top_level_category_loader
+            yield request
+
+    def parse_sub(self, response):
+        top_level_category_loader = response.meta['top_level_category_loader']
+        parent_category_loader = top_level_category_loader
+
+        for category_sel in response.xpath('//div[@id="divCategories"]/div/ul[@class="categories"]/li/a'):
+            category_loader = self.generate_loader(category_sel, response)
+            parent_category_loader.add_value('sub_categories', category_loader.load_item())
+
+        yield top_level_category_loader.load_item()
