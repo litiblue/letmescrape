95348707cd7b8b2c77e8109f739e372b5341d6e5
junseok.seo <junseok.seo@friberry.com>
2015-08-26 16:47:27 +0900

    [ralphlauren] SALE의 일부 하위 카테고리를 가져오지 못하는 문제 해결

--- letmescrape/spiders/ralphlauren_category.py
+++ letmescrape/spiders/ralphlauren_category.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from base import CategorySpider
 from letmescrape.loaders import CategoryLoader
 from letmescrape.processors import JoinExcludingEmptyValues
 from letmescrape.scripts import make_lua_script
 from scrapy import Request
+
 
 class RalphlaurenCategorySpider(CategorySpider):
     name = "ralphlauren_category"
@@ -14,7 +14,6 @@ class RalphlaurenCategorySpider(CategorySpider):
     )
 
     def parse(self, response):
-
         selector_list = ["li[rel=shop-all-men]"]
         script = make_lua_script(selector_list)
         request = Request(response.url, callback=self.parse_redirect_url, meta={
@@ -26,52 +25,34 @@ class RalphlaurenCategorySpider(CategorySpider):
 
         yield request
 
-    def sale_category_loader(self, selector, response, title, link):
-        loader = CategoryLoader(selector=selector, response=response)
-        loader.title_out = JoinExcludingEmptyValues(' ')
-        loader.add_value('title', title)
-        loader.add_value('link', link)
-
-        return loader
-
-
     def parse_redirect_url(self, response):
-
         def _generate_loader(selector):
             loader = CategoryLoader(selector=selector, response=response)
             loader.title_out = JoinExcludingEmptyValues(' ')
-            loader.add_xpath('title', '@title')
-            loader.add_xpath('title', 'h3/text()')
-            loader.add_xpath('title', 'a/text()')
-            loader.add_xpath('title', 'a/font/b/text()')
+            loader.add_xpath('title', 'text()')
+            loader.add_xpath('title', 'font/b/text()')
+            loader.add_xpath('title', 'b/font/text()')
+            loader.add_xpath('title', 'font/text()')
+            loader.add_xpath('title', 'span/text()')
             loader.add_xpath('link', '@href')
             loader.add_xpath('link', 'a/@href')
-
             return loader
 
         for top_level_sel in response.xpath('//ul[@id="global-nav"]/li[contains(@class, "navitem  more")]'):
             exist_category = top_level_sel.xpath('a[not(contains(@class,"top-nav last"))]').extract()
 
             if len(exist_category) != 0:
-                top_level_category_loader =_generate_loader(top_level_sel.xpath('a[not(contains(@class,"top-nav last"))]'))
+                top_level_category_loader =_generate_loader(
+                    top_level_sel.xpath('a[not(contains(@class,"top-nav last"))]'))
                 yield top_level_category_loader.load_item()
 
-            parent_category_loader = top_level_category_loader
-
             for column_sel in top_level_sel.xpath('div/div'):
-                category_candidate = column_sel.xpath('h3/text()').extract()
-
-                if category_candidate:
-                    category_loader = _generate_loader(column_sel)
-                    category_loader.add_value('parent_loader', parent_category_loader)
+                for idx, category_sel in enumerate(column_sel.xpath('h3')):
+                    category_loader = _generate_loader(category_sel)
+                    category_loader.add_value('parent_loader', top_level_category_loader)
                     yield category_loader.load_item()
 
-                    for leaf_sel in column_sel.xpath('ul[@class="nav-items"]/li'):
-
-                        exist_leaf_sel_title_text = leaf_sel.xpath('a/text()').extract()
-                        exist_sale_category = leaf_sel.xpath('a/font/b/text()').extract()
-
-                        if len(exist_leaf_sel_title_text) != 0 or len(exist_sale_category):
-                            leaf_loader = _generate_loader(leaf_sel)
-                            leaf_loader.add_value('parent_loader', category_loader)
-                            yield leaf_loader.load_item()
+                    for leaf_sel in category_sel.xpath('../ul[%d]/li/a' % (idx+1)):
+                        leaf_loader = _generate_loader(leaf_sel)
+                        leaf_loader.add_value('parent_loader', category_loader)
+                        yield leaf_loader.load_item()
